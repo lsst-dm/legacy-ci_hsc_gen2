4ca41b5e585f04fa71b290343c29f34a4b39d017
Paul Price <price@astro.princeton.edu>
2020-01-30 22:11:05 -0500

    disable external calibs for forcedPhotCcd
    These have been activated in obs_subaru, but we don't run
    jointcal here, so need to deactivate them.

--- forcedPhotCcdConfig.py
+++ forcedPhotCcdConfig.py
@@ -1,3 +1,7 @@
 # We haven't processed all the patches that overlap some of our CCDs
 # to save some time.
 config.references.skipMissing = True
+
+# We don't run jointcal, so can't load its results
+config.recalibrate.doApplyExternalPhotoCalib = False
+config.recalibrate.doApplyExternalSkyWcs = False
